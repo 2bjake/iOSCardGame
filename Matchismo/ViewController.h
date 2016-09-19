457d3b05c98a1dc9cdcf749fd4e3443b9a4f69b6
2bjake <fosterjake@gmail.com>
2016-09-19 13:48:58 -0500

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Foster, Jake on 9/19/16.
+//  Copyright © 2016 Amazon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Matchismo
+//
+//  Created by Foster, Jake on 9/19/16.
+//  Copyright © 2016 Amazon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
